b26e7c910b92565eb867f0da3dcc6c97dca3416e
shashu786 <syed.shashu@gmail.com>
2023-02-20 08:24:53 +0530

    department service code commit

---
+++ department-service/src/main/java/com/microservices/departmentservice/controller/DepartmentController.java
@@ -0,0 +1,27 @@
+package com.microservices.departmentservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microservices.departmentservice.entity.Department;
+import com.microservices.departmentservice.service.DepartmentService;
+
+@RestController
+@RequestMapping("/departments")
+public class DepartmentController {
+	@Autowired
+	private DepartmentService departmentService;
+	@PostMapping("/")
+	public Department saveDeparment(@RequestBody Department department) {
+		return departmentService.saveDepartment(department);
+	}
+	@GetMapping("/{id}")
+	public Department findDepartmentById(@PathVariable("id") Long departmentId) {
+		return departmentService.findDepartmentById(departmentId);
+	}
+}
---
+++ department-service/src/main/java/com/microservices/departmentservice/repository/DepartmentRepository.java
@@ -0,0 +1,10 @@
+package com.microservices.departmentservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.microservices.departmentservice.entity.Department;
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+}
